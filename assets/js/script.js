c4b16295a3e4ff26950373593b0698e6b726950d
nickrosales <116924957+nickrosales@users.noreply.github.com>
2022-12-11 18:18:31 -0700

    added comments to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 
-
+//stores the cutrrent date into a variable
 var currentDate = dayjs().format('MMMM D, YYYY');
+//saves the save buttons to a variable
 var saveBtn = document.querySelector('.saveBtn')
+// saves the textarea in each timeblock to a variable
 var nineBlock = document.querySelector('#h9t');
 var tenBlock = document.querySelector('#h10t');
 var elevenBlock = document.querySelector('#h11t');
@@ -11,10 +13,11 @@ var twoBlock = document.querySelector('#h14t');
 var threeBlock = document.querySelector('#h15t');
 var fourBlock = document.querySelector('#h16t');
 var fiveBlock = document.querySelector('#h17t');
-
+//displays the current date in the header
 $('#currentDay').text('Today is: ' + currentDate);
-
+//stores the current time wiht hour only on 24 hour clock
 var currentTime = dayjs().format('HH');
+//loops to add differnt classes to each time block depending on the time
 for(i = 9; i < 18; i++) {
   if(currentTime < i) {
     $('#hour-' + i).addClass('future');
@@ -25,6 +28,7 @@ for(i = 9; i < 18; i++) {
   };
 };
 
+//function to save all user input to an object. then send the object to local storage
 function saveMsg() {
   var calender = {
     nineBlockMsg: nineBlock.value.trim(),
@@ -40,6 +44,7 @@ function saveMsg() {
   localStorage.setItem('calender', JSON.stringify(calender));
 };
 
+//loads object from loacal storage into the user input areas
 function renderMsg () {
   var lastMsg = JSON.parse(localStorage.getItem('calender'));
   if(lastMsg !== null) {
@@ -56,15 +61,15 @@ function renderMsg () {
     return;
   };
 };
-
+//listens for a click on the save buttons then runs the save and reder msg funtions
 saveBtn.addEventListener('click', function(event) {
   event.preventDefault();
   saveMsg();
   renderMsg();
 });
 
+//runs the renderMsg funtion whgen the page loads
 function init() {
   renderMsg();
 };
-
 init();
